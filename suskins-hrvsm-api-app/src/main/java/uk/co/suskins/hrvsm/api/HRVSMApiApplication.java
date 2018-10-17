4e9a0f0afe34d66f85d976a136b1c42f8088fd27
Ben Suskins <benbuskins@gmail.com>
2018-10-17 16:06:23 +0100

    HRVSM-22 Created IntelliJ project

---
+++ suskins-hrvsm-api-app/src/main/java/uk/co/suskins/hrvsm/api/HRVSMApiApplication.java
@@ -0,0 +1,10 @@
+package uk.co.suskins.hrvsm.api;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HRVSMApiApplication {
+
+    public static void main(String args[]) {
+    }
+}
